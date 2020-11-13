a82a67399b07812fa4527ff1f7a69d204f444808
unknown <bucky@pornhub.gov>
2020-11-13 14:01:45 +0900

    db

--- index.js
+++ index.js
@@ -190,7 +190,7 @@ client.on('message', (message) => {
     var isNum = !isNaN(clearLine)
 
     if(isNum && (clearLine <= 0 || 100 < clearLine)) {
-      message.channel.send("1부터 100까지의 숫자만 입력해주세요.")
+      message.channel.send("1부터 99까지의 숫자만 입력해주세요.")
       return;
     } else if(!isNum) { // c @나긋해 3
       if(message.content.split('<@').length == 2) {
